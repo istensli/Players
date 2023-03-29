2a3d25af1f8bee05d62755cf8aa9e66aae471e41
istensli <istensli@gmail.com>
2023-03-29 11:09:42 +0200

    Initial commit

---
+++ Players/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+    /*
+     * Create Player class
+     *      - name
+     *      - points
+     *      - constructor
+     *          - name
+     *          - points
+     *      - Play(Player otherPlayer)
+     *          - create instance of random
+     *          - assigns one player as winner randomly
+     *              - add point to winner
+     *              - remove point from loser
+     *      - ShowNameAndPoints()
+     *          - console out name and points
+     */
+namespace Players
+{
+    internal class Player
+    {
+        private readonly string _name;
+        private int _points;
+
+        public Player(string name, int points)
+        {
+            _name = name;
+            _points = points;
+        }
+        public void Play(Player otherPlayer)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(2);
+
+            if (randomNumber == 0)
+            {
+                _points++;
+                if (otherPlayer._points > 0) otherPlayer._points--;
+            }
+            else
+            {
+                otherPlayer._points++;
+                if (_points > 0) _points--;
+            }
+        }
+        public void ShowNameAndPoints()
+        {
+            Console.WriteLine($"Name: {_name} Points: {_points}");
+        }
+    }
+}
